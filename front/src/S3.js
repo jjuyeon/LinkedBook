d5fb30f6cdb7fac379dcbde5c3cc7a31d61b41fd
daewoong <dwbyun16@gmail.com>
2021-08-31 16:51:50 +0900

    Update S3.js

--- front/src/S3.js
+++ front/src/S3.js
@@ -4,8 +4,8 @@ const config = {
   bucketName: "linkedbook",
   dirName: "images" /* optional */,
   region: "ap-northeast-2",
-  accessKeyId: "AKIA2VNVQ3CIW7RQRMVC",
-  secretAccessKey: "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",
+  accessKeyId: "accessKeyId",
+  secretAccessKey: "secretAccessKey",
 };
 
 const ReactS3Client = new S3(config);
